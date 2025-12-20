57f7160b7dd8b7d85e8e6d5cc37ed47e850e0e3f
Akshansh-29072005 <akshanshkhairwar@gmail.com>
2025-12-21 01:23:16 +0530

    created the first endpoint

--- backend/cmd/aarcs-x_backend/main.go
+++ backend/cmd/aarcs-x_backend/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Akshansh-29072005/AARCS-X/backend/internals/config"
+)
 
 func main(){
-	fmt.Println("Welcome to the AARCS-X Backend")
+	//load config
+	cfg := config.MustLoad()
+	//database setup
+	//router setup
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the AARCS-X backend"))
+	})
+	//server setup
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+	fmt.Printf("Server Started %s",cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+	if err !=nil{
+		log.Fatal("Failed to start the server")
+	}
 }
