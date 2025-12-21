222a1198cfe62b0f56b1be8510711e28cade864b
Akshansh-29072005 <akshanshkhairwar@gmail.com>
2025-12-21 14:52:11 +0530

    added graceful shutdown, go routine and channel

--- backend/cmd/aarcs-x_backend/main.go
+++ backend/cmd/aarcs-x_backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Akshansh-29072005/AARCS-X/backend/internals/config"
 )
@@ -22,9 +27,38 @@ func main(){
 		Addr: cfg.Addr,
 		Handler: router,
 	}
-	fmt.Printf("Server Started %s",cfg.HTTPServer.Addr)
-	err := server.ListenAndServe()
-	if err !=nil{
-		log.Fatal("Failed to start the server")
+	
+	slog.Info("Server started", slog.String("Address",cfg.Addr))
+
+	//created a channel for the server to run
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	
+	go func ()  {
+		err := server.ListenAndServe()
+		if err !=nil{
+			log.Fatal("Failed to start the server")
+		}
+	} ()
+
+	<-done
+
+	slog.Info("Shutting down the server!")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+
+	if err != nil{
+		slog.Error("Failed to shutdown the server", slog.String("error", err.Error()))
 	}
+
+	//Above lines can also be written as below also!
+	// if err := server.Shutdown(ctx); err != nil{
+	// 	slog.Error("Failed to shutdown the server", slog.String("error", err.Error()))
+	// }
+
+	slog.Info("Server shutdown sucessfully!")
 }
