19c4980369aa17a009f3afed38e31cd28026b9db
Akshansh-29072005 <akshanshkhairwar@gmail.com>
2026-01-03 18:52:07 +0530

    feat: add initial configuration and main application setup for AARCS-X API

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,80 +1,19 @@
 package main
 
-import (
-	"context"
-	"log"
-	"log/slog"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+import "github.com/gin-gonic/gin"
 
-	"github.com/Akshansh-29072005/AARCS-X/backend/internals/config"
-	"github.com/Akshansh-29072005/AARCS-X/backend/internals/http/handlers/student"
-	"github.com/Akshansh-29072005/AARCS-X/backend/internals/storage/sqlite"
-)
+func main() {
+	var router *gin.Engine = gin.Default()
 
-func main(){
-	//load config
-	cfg := config.MustLoad()
-	//database setup
+	router.SetTrustedProxies(nil)
 
-	storage, err := sqlite.New(cfg)
-	if err != nil{
-		log.Fatal(err)
-	}
+	router.GET("/", func(c *gin.Context){
+		//map[string]interface{}
+		c.JSON(200, gin.H{
+			"message" : "AARCS-X API is running!",
+			"status" : "success",
+		})
+	})
 
-	slog.Info("storage intialized", slog.String("env", cfg.Env), slog.String("version","1.0.0"))
-
-	//router setup
-	//This route is used for making new students.
-	router := http.NewServeMux()
-	//This route is used for creating a new student.
-	router.HandleFunc("POST /api/students", student.New(storage))
-	//This route is used for extracting the student's info using the Id of the student.
-	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
-	//This route is used for getting the list of all students.
-	router.HandleFunc("GET /api/students", student.GetList(storage))
-	//This route is used to delete a student using the Id of the student.
-	
-	//server setup
-	server := http.Server{
-		Addr: cfg.Addr,
-		Handler: router,
-	}
-	
-	slog.Info("Server started", slog.String("Address",cfg.Addr))
-
-	//created a channel for the server to run
-	done := make(chan os.Signal, 1)
-
-	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	
-	go func ()  {
-		err := server.ListenAndServe()
-		if err !=nil{
-			log.Fatal("Failed to start the server")
-		}
-	} ()
-
-	<-done
-
-	slog.Info("Shutting down the server!")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-	defer cancel()
-
-	err = server.Shutdown(ctx)
-
-	if err != nil{
-		slog.Error("Failed to shutdown the server", slog.String("error", err.Error()))
-	}
-
-	//Above lines can also be written as below also!
-	// if err := server.Shutdown(ctx); err != nil{
-	// 	slog.Error("Failed to shutdown the server", slog.String("error", err.Error()))
-	// }
-
-	slog.Info("Server shutdown sucessfully!")
+	router.Run(":3000")
 }
