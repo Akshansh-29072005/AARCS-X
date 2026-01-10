4741cdeeeef1becd2619a9d8c618930a7f4f9324
Akshansh-29072005 <akshanshkhairwar@gmail.com>
2026-01-11 00:07:26 +0530

    feat: Add routing for student and teacher management with respective handlers

--- backend/internal/students/routes.go
+++ backend/internal/students/routes.go
@@ -1,1 +1,11 @@
 package students
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func StudentRoutes(router *gin.Engine, pool *pgxpool.Pool) {
+	router.POST("/api/students", CreateStudentHandler(pool))
+	router.GET("/api/students", GetStudentHandler(pool))
+}
--- backend/internal/teachers/routes.go
+++ backend/internal/teachers/routes.go
@@ -1,1 +1,15 @@
 package teachers
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func TeacherRoutes(router *gin.Engine, pool *pgxpool.Pool) {
+
+	// Create Teacher Route
+	router.POST("/api/teachers", CreateTeacherHandler(pool))
+
+	// Get Teachers Route
+	// router.GET("/api/teachers", GetTeacherHandler(pool))
+}
