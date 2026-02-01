37d07e9c5454d70153a18909f9a744e2f5e35567
Akshansh-29072005 <akshanshkhairwar@gmail.com>
2026-02-01 14:53:02 +0530

    feat: Add authentication routes for user registration, login, and protected user info

--- backend/internal/auth/routes.go
+++ backend/internal/auth/routes.go
@@ -1,1 +1,23 @@
 package auth
+
+import "github.com/gin-gonic/gin"
+
+func RegisteredRoutes(r *gin.Engine, h *Handler) {
+
+	api := r.Group("/api/v1/auth")
+
+	// User Registering Route
+	api.POST("/register", h.RegisterUser)
+
+	// User Logging In Route
+	api.POST("/login", h.Login)
+
+	protected := api.Group("/protected")
+	protected.Use(AuthMiddleware())
+
+	// User Info Route
+	protected.GET("/me", h.Me)
+
+	// User Logging Out Route
+	protected.POST("/logout", h.Logout)
+}
