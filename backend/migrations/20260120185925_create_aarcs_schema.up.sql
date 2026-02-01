7b008e0d7493fc5f2236b16a49cda226a6a90267
Akshansh-29072005 <akshanshkhairwar@gmail.com>
2026-02-01 15:02:06 +0530

    feat: Update users table to make reference_id optional and add role constraints

--- backend/migrations/20260120185925_create_aarcs_schema.up.sql
+++ backend/migrations/20260120185925_create_aarcs_schema.up.sql
@@ -67,7 +67,7 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role TEXT CHECK (role IN ('institution', 'teacher', 'student')) NOT NULL,
-    reference_id INT NOT NULL,
+    reference_id INT,
     created_at TIMESTAMP DEFAULT now()
 );
 
