724ea803e43552a62c30a23b2d6d87fc1a489877
Akshansh-29072005 <akshanshkhairwar@gmail.com>
2026-02-24 20:29:20 +0530

    feat: Add authentication middleware and role requirement to institution routes

--- backend/internal/institutes/routes.go
+++ backend/internal/institutes/routes.go
@@ -1,10 +1,15 @@
 package institutes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Akshansh-29072005/AARCS-X/backend/internal/platform/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(r *gin.Engine, h *Handler) {
 
 	group := r.Group("/api/v1")
+	group.Use(middleware.AuthMiddleware())
+	group.Use(middleware.RequireRole("institution"))
 
 	// Student Creating Route
 	group.POST("/institutions", h.CreateInstitute)
